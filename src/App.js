973bb6518f3a96f9fa074106b04b0595f3ac97bb
BorisStankov98 <boris.stankovv@gmail.com>
2024-09-16 15:53:09 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Scene } from '@soulmachines/smwebsdk';
+
+async function createScene(){
+  const scene = new Scene({
+    apiKey: 'eyJzb3VsSWQiOiJkZG5hLWtiYy0tZXhwbG9yZXItNjczM2M3ZiIsImF1dGhTZXJ2ZXIiOiJodHRwczovL2RoLnNvdWxtYWNoaW5lcy5jbG91ZC9hcGkvand0IiwiYXV0aFRva2VuIjoiYXBpa2V5X3YxX2I5NzRjY2FiLTNiNDEtNGFkNS1iZjhlLWFiYzNmOWQzNzUwZiJ9',
+  });
+  
+  await scene.connect();
+}
+
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +20,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          
+        <video id="sm-video" width="100%" height="100%"></video>
+        <button onClick={createScene}>generate scene</button>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
