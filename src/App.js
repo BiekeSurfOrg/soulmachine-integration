afb64ac2814b8e33b3cc5e985e44d446c0754de4
BorisStankov98 <boris.stankovv@gmail.com>
2024-09-17 12:30:14 +0300

    changed the logic from button click to automatic load on component render

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import { createScene } from './soulIntegration'; 
+import { useEffect } from 'react';
+
 function App() {
- 
+
+  useEffect(() => {
+   createScene()
+  }, []); 
+
   return (
-    <div className="App">
-          
+    <div className="App">        
           <video id="sm-video" width="100%" height="100%"></video>
-        <button onClick={createScene}>generate scene</button>
-  
     </div>
   );
 }
