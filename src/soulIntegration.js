7dc458d2057c1e5fcdecac7fbbe6351bf70010f1
BorisStankov98 <boris.stankovv@gmail.com>
2024-09-17 12:16:33 +0300

    new dimensions and api key

--- src/soulIntegration.js
+++ src/soulIntegration.js
@@ -7,7 +7,7 @@ export async function createScene(){
 
    scene = new Scene({
     videoElement: videoEl,
-    apiKey: 'eyJzb3VsSWQiOiJkZG5hLWtiYy0tZXhwbG9yZXItNjczM2M3ZiIsImF1dGhTZXJ2ZXIiOiJodHRwczovL2RoLnNvdWxtYWNoaW5lcy5jbG91ZC9hcGkvand0IiwiYXV0aFRva2VuIjoiYXBpa2V5X3YxX2I5NzRjY2FiLTNiNDEtNGFkNS1iZjhlLWFiYzNmOWQzNzUwZiJ9',
+    apiKey: 'eyJzb3VsSWQiOiJkZG5hLWtiYy0tZXhwbG9yZXItNjczM2M3ZiIsImF1dGhTZXJ2ZXIiOiJodHRwczovL2RoLnNvdWxtYWNoaW5lcy5jbG91ZC9hcGkvand0IiwiYXV0aFRva2VuIjoiYXBpa2V5X3YxX2E0ZGQ5ZjM1LWFiMmQtNGM2Ni05MTE1LWYwODc5YTdlNWMzOCJ9',
     requestedMediaDevices: { microphone: true, camera: true },
     requiredMediaDevices: { microphone: true, camera: true },
   });
