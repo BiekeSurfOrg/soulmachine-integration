4d8122d02c39d8d8f56c89358ee7b5149e774774
jd12338 <thomas.rotte@kbc.be>
2024-10-08 10:39:41 +0200

    try to add link

--- src/App.js
+++ src/App.js
@@ -3,10 +3,39 @@ import Soul from './Soul';
 
 function App() {
 
+    function goToMeeting() {
+        call = callAgent.join({ meetingLink: MEETING_LINK }, {});
+
+        call.on('stateChanged', () => {
+          callStateElement.innerText = call.state;
+        })
+
+        call.feature(Features.Recording).on('isRecordingActiveChanged', () => {
+          if (call.feature(Features.Recording).isRecordingActive) {
+            recordingStateElement.innerText = "This call is being recorded";
+          }
+          else {
+            recordingStateElement.innerText = "";
+          }
+        });
+        setButtonDisable(false);
+
+        teamsMeetingJoinButton.disabled = true;
+      }
 
   return (
     <div className="App">
-        <a data-sm-content="teamsLink" href="https://teams-integration-f1a331d78992.herokuapp.com/" alt="teamslink"> click here </a>
+            <div data-sm-content="teamsLink">
+              {buttonDisable ? <button
+                className="kbc-style-button"
+                id="join-meeting-button"
+                type="button"
+                disabled={false} onClick={() => goToMeeting()}
+              >
+                Call a human
+              </button>
+              }
+             </div>
        <Soul></Soul>
     </div>
   );
